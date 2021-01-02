91bae5fee6b41d17748c7d628fe71acd0f9bfcb0
DELL <21AK22@DELL.DELL>
2021-01-02 13:36:37 +0700

    plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 // webpack.config.js
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const  CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
     // 1. [context]: src is the folder with all the
   // sources and so that webpack would watch all
@@ -16,7 +19,19 @@ module.exports = {
 
   //4. [output]: the path and file name where we will add all our javascript code.
   output: {
-      filename: 'bundle.js',
+      filename: 'bundle.[hash].js',
       path: path.resolve(__dirname,'dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({template: 'index.html'}),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        }
+      ]
+    })
+  ]
 }
